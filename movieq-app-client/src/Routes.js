5260801539cd6d47d4fee001a4ef56240335633d
Danie Viljoen <danie.viljoen@orionhealth.com>
2017-05-28 10:28:24 +1200

    add Login

--- movieq-app-client/src/Routes.js
+++ movieq-app-client/src/Routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Login from './containers/Login';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from './containers/NotFound';
 import Home from './containers/Home';
@@ -6,6 +7,7 @@ import Home from './containers/Home';
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>
