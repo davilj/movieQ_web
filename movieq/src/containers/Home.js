65d756c5d3ec205c225eb6b6604fed2ec54198b0
Danie Viljoen <danie.viljoen@orionhealth.com>
2017-07-10 20:46:21 +1200

    Home container

---
+++ movieq/src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Movie Q</h1>
+          <p>Test you movie knowledge</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
