2a690385b03d16011e4d399c6bf24b93466a6005
Danie Viljoen <danie.viljoen@orionhealth.com>
2017-07-11 20:04:37 +1200

    signup

--- movieq/src/Routes.js
+++ movieq/src/Routes.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Login from './containers/Login';
 import { Route, Switch } from 'react-router-dom';
-
-import Home from './containers/Home';
 import NotFound from './containers/NotFound';
+import Home from './containers/Home';
+import AppliedRoute from './components/AppliedRoute';
+import Signup from './containers/Signup';
 
-export default () => (
+
+export default ({ childProps }) => (
   <Switch>
-    <Route path="/" exact component={Home} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
 
     <Route component={NotFound} />
   </Switch>
---
+++ movieq/src/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiGateway: {
+  URL: 'https://rsmvlze18i.execute-api.us-east-1.amazonaws.com/dev',
+  },
+  cognito: {
+    USER_POOL_ID : 'us-east-1_R2YsaKoBf',
+    APP_CLIENT_ID : '2gqni39d218bm97na1gq3r8esj',
+  }
+};
